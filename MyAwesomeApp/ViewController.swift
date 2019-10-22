c2f0d2a0eedf9ba826bb5efeb892d6ecf87039df
CernohorskyVaclav <vaclav.cernohorsky@gmail.com>
2019-10-22 13:44:28 +0200

    Initial Commit

---
+++ MyAwesomeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAwesomeApp
+//
+//  Created by Václav Černohorský on 22/10/2019.
+//  Copyright © 2019 Václav Černohorský. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
